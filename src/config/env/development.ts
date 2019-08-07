9a7daa8161c251594d4cc330d67b6fe5b6358efd
hongjongwan <serverwizard89@woowahan.com>
2019-08-07 17:52:23 +0900

    환경별 URL 변경

--- src/config/env/development.ts
+++ src/config/env/development.ts
@@ -5,8 +5,10 @@ let all = {
 
 // TODO 환경설정파일을 별도로 관리한다면 어떤식으로 관리하는지 확인할 것
 const HOST: string = {
-	dev: "http://api.inbound.smartmenu.dev.woowa.in",
-	beta: "http://api.inbound.smartmenu.dev.woowa.in",
-	stage: "http://api.inbound.smartmenu.stage.woowa.in",
-	prod: "http://api.inbound.smartmenu.woowa.in"
+	dev: "http://api.dev.woowa.in",
+	beta: "http://api.dev.woowa.in",
+	stage: "http://api.stage.woowa.in",
+	prod: "http://api..woowa.in"
 }[process.env.PROFILE || 'prod'];
+
+// TODO https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
